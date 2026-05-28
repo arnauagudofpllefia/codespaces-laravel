0a95bd2ae3fd4db18ab4ee35084bcdd57f68d5bc
Arnau Agudo <arnagudo05@gmail.com>
2026-05-28 10:17:31 +0000

    cambio cors temporal

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'storage/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'https://gymnau.vercel.app')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost')],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
